d6c822f391376ec183bac23948f332174af03f5d
Michal Gumny <michalgumny@outlook.com>
2018-05-04 18:51:30 +0200

    Make make run on multi core

--- build-gcc.sh
+++ build-gcc.sh
@@ -6,4 +6,4 @@ CMAKE_CXX_COMPILER="/usr/local/Cellar/gcc/7.3.0_1/bin/g++-7"
 
 cd cmake-build-debug-gcc
 cmake -DCMAKE_C_COMPILER=$CMAKE_C_COMPILER -DCMAKE_CXX_COMPILER=$CMAKE_CXX_COMPILER -DCMAKE_CXX_FLAGS=$CXX_FLAGS -DCMAKE_BUILD_TYPE=Debug ..
-make
+make -j4
